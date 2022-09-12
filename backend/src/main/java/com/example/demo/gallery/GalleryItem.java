a76fb3b2512f2a1be058c4dfd8652e44fb0ac9a5
Gregor499 <92386941+Gregor499@users.noreply.github.com>
2022-09-12 14:08:37 +0200

    initial project setup
    still need to remove parent annotation from backend folder and weirdly generated frontend parent folder

---
+++ backend/src/main/java/com/example/demo/gallery/GalleryItem.java
@@ -0,0 +1,4 @@
+package com.example.demo.gallery;
+
+public class GalleryItem {
+}
---
+++ backend/src/test/java/com/example/demo/pictureGallery/TimeTableGeneratorIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.pictureGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TimeTableGeneratorIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void integrationTest() {
+
+    }
+}
